ee846ae68183757f23c70ea30c15db99e5646217
Sekhar V <sekhar@gmail.com>
2025-12-16 09:03:15 +0530

    Second commit

---
+++ src/ex_Lab001_BasicJavaPrograms/Lab002_MultpleMainMethods.java
@@ -0,0 +1,16 @@
+package ex_Lab001_BasicJavaPrograms;
+
+public class Lab002_MultpleMainMethods {
+    public static void main(String[] args) {
+
+        System.out.println("This is the main method");
+    }
+
+    public static void main(String[] sek) {
+
+    }
+
+    public static void main(String args) {
+
+    }
+}
