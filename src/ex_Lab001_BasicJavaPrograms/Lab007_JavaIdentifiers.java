8bb7189e56ad691b5b87e2c81121c435ed50ea05
Sekhar V <sekhar@gmail.com>
2025-12-16 09:44:49 +0530

    Here i have wrote examples with correct way and wrong in identifierrs examples

---
+++ src/ex_Lab001_BasicJavaPrograms/Lab007_JavaIdentifiers.java
@@ -0,0 +1,26 @@
+package ex_Lab001_BasicJavaPrograms;
+// This is Java keyword program and it has 50 reserved keywords as for JDK 17.
+/*
+
+ */
+
+public class Lab007_JavaIdentifiers {
+
+    public static void main(String[] args) {
+
+        System.out.println(" Identifiers Examples with Correct Vs Wrong");
+
+        int age;
+        String userName;
+        double salary50000;
+        int count;
+        int $count;
+
+        /*
+        int 2Age;
+        String User Name;
+        int class;
+        int @price;
+         */
+    }
+}
