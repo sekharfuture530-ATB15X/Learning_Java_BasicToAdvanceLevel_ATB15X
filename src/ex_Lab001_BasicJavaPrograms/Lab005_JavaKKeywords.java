efadfb64d4cd204a707009100927f8f5fb62173f
Sekhar V <sekhar@gmail.com>
2025-12-16 09:28:26 +0530

    This is java JDK 17 reserved keywords 50

---
+++ src/ex_Lab001_BasicJavaPrograms/Lab005_JavaKKeywords.java
@@ -0,0 +1,26 @@
+package ex_Lab001_BasicJavaPrograms;
+// This is Java keyword program and it has 50 reserved keywords as for JDK 17.
+
+/*
+abstract     assert        boolean      break
+byte         case          catch        char
+class        continue      default      do
+double       else          enum         extends
+final        finally       float        for
+if           implements    import       instanceof
+int          interface     long
+native       new           package      private
+protected    public        return       short
+static       strictfp      super         switch
+synchronized this          throw         throws
+transient    try           void          volatile
+while
+
+ */
+public class Lab005_JavaKKeywords {
+
+    public static void main(String[] args) {
+
+        System.out.println("This is Java keyword in JDK 24 has 53 to 58");
+    }
+}
