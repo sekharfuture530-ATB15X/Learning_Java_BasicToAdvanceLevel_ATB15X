e3101e1ff08fb8b49114a85e523b301879fa6156
Sekhar V <sekhar@gmail.com>
2025-12-16 09:30:32 +0530

    I have updated to courseBatch instead of CourseName

--- src/ex_Lab001_BasicJavaPrograms/Lab004_MutilpeComments.java
+++ src/ex_Lab001_BasicJavaPrograms/Lab004_MutilpeComments.java
@@ -3,7 +3,7 @@ package ex_Lab001_BasicJavaPrograms;
 /**
  * Author : Sekhar
  * Course : Java
- * CourseName : ATB15X
+ * CourseBatch : ATB15X
  */
 public class Lab004_MutilpeComments {
 
@@ -11,14 +11,14 @@ public class Lab004_MutilpeComments {
     /**
      * Author : Sekhar
      * Course : Java
-     * CourseName : ATB15X
+     * CourseBatch : ATB15X
      */
     public static void main(String[] args) {
 
         /**
          * Author : Sekhar
          * Course : Java
-         * CourseName : ATB15X
+         * CourseBatch : ATB15X
          */
         System.out.println("This is Mutiple comments");
     }
