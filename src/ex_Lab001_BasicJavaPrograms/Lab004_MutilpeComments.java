9b45fe62c4ebd3e1bf631f82f1cd3a97e82ba8f8
Sekhar V <sekhar@gmail.com>
2025-12-16 09:21:27 +0530

    This is Mutiple Comments & commit

---
+++ src/ex_Lab001_BasicJavaPrograms/Lab004_MutilpeComments.java
@@ -0,0 +1,25 @@
+package ex_Lab001_BasicJavaPrograms;
+
+/**
+ * Author : Sekhar
+ * Course : Java
+ * CourseName : ATB15X
+ */
+public class Lab004_MutilpeComments {
+
+
+    /**
+     * Author : Sekhar
+     * Course : Java
+     * CourseName : ATB15X
+     */
+    public static void main(String[] args) {
+
+        /**
+         * Author : Sekhar
+         * Course : Java
+         * CourseName : ATB15X
+         */
+        System.out.println("This is Mutiple comments");
+    }
+}
