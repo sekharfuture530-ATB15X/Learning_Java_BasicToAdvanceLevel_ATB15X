7677d648e7a0c4f97266d4c54fdceaaf76e7953f
Sekhar V <sekhar@gmail.com>
2025-12-16 09:17:25 +0530

    This is Single Comments commit

---
+++ src/ex_Lab001_BasicJavaPrograms/Lab003_SingleComments.java
@@ -0,0 +1,11 @@
+package ex_Lab001_BasicJavaPrograms;
+
+public class Lab003_SingleComments {
+
+//    This is single comments with Lab003_SingleComments
+//    This is my first program for single comments in the furture will do more on this and it will understand juniors.
+    public static void main(String[] args) {
+
+        System.out.println("This is single comments");
+    }
+}
