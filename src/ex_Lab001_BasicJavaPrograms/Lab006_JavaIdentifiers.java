6b1618c3a5f640f40150e79ebeeaaae0694550c2
Sekhar V <sekhar@gmail.com>
2025-12-16 09:38:20 +0530

    This is Java Identifiers program with rules means allowed and not allowed with examples

---
+++ src/ex_Lab001_BasicJavaPrograms/Lab006_JavaIdentifiers.java
@@ -0,0 +1,38 @@
+package ex_Lab001_BasicJavaPrograms;
+// This is Java keyword program and it has 50 reserved keywords as for JDK 17.
+/*
+Identifiers are names given by the programmer
+Like....
+Classes
+Methods
+Variables
+Interfaces
+Packages
+
+Please follow the Rules: I mean how to follow the rules and regulation as for the standarded
+
+✅ Allowed
+
+✔ Letters (A-Z, a-z)
+✔ Digits (0-9) (not first)
+✔ _ (underscore)
+✔ $ (dollar sign)
+
+❌ Not Allowed
+
+❌ Start with digit
+❌ Use spaces
+❌ Use special symbols (@ # %)
+❌ Use Java keywords
+
+
+
+ */
+
+public class Lab006_JavaIdentifiers {
+
+    public static void main(String[] args) {
+
+        System.out.println("This is programs belong to java identifiers..");
+    }
+}
