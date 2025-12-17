9ab79d4ffaf5f4274969e2f50bee48c2108fc913
Sekhar V <sekharfurture530@gmail.com>
2025-12-17 13:02:55 +0530

    Adding datatypes variables like byte,short,int,long and char,boolean,double,float.

---
+++ src/ex_Lab002_Variables_DataTypes/Lab011_DataTypes_Variable.java
@@ -0,0 +1,28 @@
+package ex_Lab002_Variables_DataTypes;
+
+public class Lab011_DataTypes_Variable {
+
+    public static void main(String[] args) {
+
+//        primitive Data types
+        byte a = 10;
+        short b = 10;
+        int c = 10;
+        long l = 98745625;
+        boolean age = true;
+        double sal = 50000;
+        float dec = 123;
+        char letter = 'A';
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(l);
+        System.out.println(age);
+        System.out.println(sal);
+        System.out.println(dec);
+        System.out.println(letter);
+
+
+    }
+}
---
+++ src/ex_Lab002_Variables_DataTypes/Lab012_DataTypes_Variable.java
@@ -0,0 +1,28 @@
+package ex_Lab002_Variables_DataTypes;
+
+public class Lab012_DataTypes_Variable {
+
+    public static void main(String[] args) {
+
+//        primitive Data types
+        byte a = 150; //overcome it means outof range
+        short b = 123456;
+        int c = 100;
+        long l = 98745625789565L;
+        boolean age = false;
+        double sal = 50000;
+        float dec = 123.50f;
+        char letter = 'Ab';
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(l);
+        System.out.println(age);
+        System.out.println(sal);
+        System.out.println(dec);
+        System.out.println(letter);
+
+
+    }
+}
