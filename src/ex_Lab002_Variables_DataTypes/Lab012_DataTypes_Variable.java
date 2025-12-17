17123273456675e62560c12f27e49fec2a95097c
Sekhar V <sekharfurture530@gmail.com>
2025-12-17 13:33:32 +0530

    Checking variables rules

--- src/ex_Lab002_Variables_DataTypes/Lab012_DataTypes_Variable.java
+++ src/ex_Lab002_Variables_DataTypes/Lab012_DataTypes_Variable.java
@@ -5,23 +5,23 @@ public class Lab012_DataTypes_Variable {
     public static void main(String[] args) {
 
 //        primitive Data types
-        byte a = 150; //overcome it means outof range
-        short b = 123456;
+//        byte a = 150; //overcome it means outof range
+//        short b = 123456;
         int c = 100;
         long l = 98745625789565L;
         boolean age = false;
         double sal = 50000;
         float dec = 123.50f;
-        char letter = 'Ab';
+//        char letter = 'Ab';
 
-        System.out.println(a);
-        System.out.println(b);
+//        System.out.println(a);
+//        System.out.println(b);
         System.out.println(c);
         System.out.println(l);
         System.out.println(age);
         System.out.println(sal);
         System.out.println(dec);
-        System.out.println(letter);
+//        System.out.println(letter);
 
 
     }
