d5538d5399718ec86d00a3c7b3f2b3edeeea6854
Sekhar V <sekharfurture530@gmail.com>
2025-12-17 16:44:04 +0530

    Adding new things like checking identifiers

---
+++ src/ex_Lab002_Variables_DataTypes/Lab016_Variables.java
@@ -0,0 +1,17 @@
+package ex_Lab002_Variables_DataTypes;
+
+public class Lab016_Variables {
+
+    static boolean a;
+    public static void main(String[] args) {
+
+        int $ =10;
+        int a = 10;
+        int b = 20;
+        char k = 10;
+        char c = 'A';
+
+
+        System.out.println(Lab016_Variables.a);
+    }
+}
