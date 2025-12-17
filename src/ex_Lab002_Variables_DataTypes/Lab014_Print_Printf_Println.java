033ba8fae52dc7778705c91c839d6ebbd1c7f316
Sekhar V <sekharfurture530@gmail.com>
2025-12-17 15:24:43 +0530

    added printf for formated outputs here apply num 9

---
+++ src/ex_Lab002_Variables_DataTypes/Lab014_Print_Printf_Println.java
@@ -0,0 +1,23 @@
+package ex_Lab002_Variables_DataTypes;
+
+public class Lab014_Print_Printf_Println {
+    public static void main(String[] args) {
+
+//        This is print : statment
+        System.out.print("Hello  " );
+        System.out.print("World");
+//        This is println statment
+        System.out.println("-------------------------------------");
+        System.out.println("Hello");
+        System.out.println("World");
+        System.out.println("------------------------------------------------");
+
+//      This is printf
+
+        int number1 = 9;
+        int number2 = 1;
+        int result = number1*number2;
+
+        System.out.println("Enter multiplication number(%d*%d)=result:"+result);
+    }
+}
---
+++ src/ex_Lab002_Variables_DataTypes/Lab015_Printf.java
@@ -0,0 +1,20 @@
+package ex_Lab002_Variables_DataTypes;
+
+public class Lab015_Printf {
+
+    public static void main(String[] args) {
+
+        int a = 9;
+        System.out.printf("%d*1=%d%n",a ,a*1);
+        System.out.printf("%d*1=%d%n",a ,a*2);
+        System.out.printf("%d*1=%d%n",a ,a*3);
+        System.out.printf("%d*1=%d%n",a ,a*4);
+        System.out.printf("%d*1=%d%n",a ,a*5);
+        System.out.printf("%d*1=%d%n",a ,a*6);
+        System.out.printf("%d*1=%d%n",a ,a*7);
+        System.out.printf("%d*1=%d%n",a ,a*8);
+        System.out.printf("%d*1=%d%n",a ,a*9);
+        System.out.printf("%d*1=%d%n",a ,a*10);
+
+    }
+}
