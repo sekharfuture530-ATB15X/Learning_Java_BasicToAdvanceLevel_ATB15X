009c6c869471b69e18ef0c1c008e9c6fdc600c91
Sekhar V <sekharfurture530@gmail.com>
2025-12-17 12:47:55 +0530

    class rename to HowtoCreateVariable

---
+++ src/ex_Lab002_Variables_DataTypes/Lab010_Howto_Create_Variables.java
@@ -0,0 +1,24 @@
+package ex_Lab002_Variables_DataTypes;
+
+/*
+ What is Variable?
+- Store the value (which can be changed in the program).
+- Each variable should be given a** unique name (**identifier).
+- Memory will be allocated for the variable while executing the program.
+- **Value of the variable** can be changed any number of times during the program execution.
+
+ */
+public class Lab010_Howto_Create_Variables {
+
+    public static void main(String[] args) {
+
+        System.out.println("Thi is Variable");
+// How to create Variables(Declaration Syntax)
+
+//        data_type variable_name = variable_value;
+
+        int age = 36;
+
+        System.out.println("My Age is :"+age);
+    }
+}
