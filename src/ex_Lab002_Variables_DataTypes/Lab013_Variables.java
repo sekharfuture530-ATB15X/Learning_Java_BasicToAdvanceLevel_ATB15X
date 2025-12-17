25a8bf41b7c40f1a0857cefe5da40092ec96a06c
Sekhar V <sekharfurture530@gmail.com>
2025-12-17 14:01:12 +0530

    Checking variables default values

---
+++ src/ex_Lab002_Variables_DataTypes/Lab013_Variables.java
@@ -0,0 +1,17 @@
+package ex_Lab002_Variables_DataTypes;
+
+public class Lab013_Variables {
+    static int age;
+    static boolean letter;
+    static double salary;
+
+    public static void main(String[] args) {
+
+        System.out.println(Lab013_Variables.letter);
+        System.out.println(Lab013_Variables.salary);
+        int _a =10;
+        int a = 0;
+        System.out.println(a);
+        System.out.println(_a);
+    }
+}
