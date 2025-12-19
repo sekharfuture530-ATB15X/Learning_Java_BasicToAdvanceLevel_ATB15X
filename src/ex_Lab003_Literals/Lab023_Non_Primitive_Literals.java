867f1c71c213f74604f346bbb29dfc56754dd981
Sekhar V <sekharfurture530@gmail.com>
2025-12-19 13:18:21 +0530

    Literals->Non_Primitive like String & Null values

---
+++ src/ex_Lab003_Literals/Lab023_Non_Primitive_Literals.java
@@ -0,0 +1,14 @@
+package ex_Lab003_Literals;
+
+public class Lab023_Non_Primitive_Literals {
+
+    public static void main(String[] args) {
+
+        String name ="Sekhar";
+
+        int[] array_name = new int[5];
+
+        char c = '0';
+        System.out.println(c);
+    }
+}
---
+++ src/ex_Lab003_Literals/Lab024_String_Null.java
@@ -0,0 +1,12 @@
+package ex_Lab003_Literals;
+
+public class Lab024_String_Null {
+    public static void main(String[] args) {
+
+        String name = null;
+        String name1 = "";
+        char c = '0';
+//        int a = null;
+        System.out.println(name);
+    }
+}
