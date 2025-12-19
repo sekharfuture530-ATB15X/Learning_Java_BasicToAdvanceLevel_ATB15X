55c9624e40271627376d75008ee45f5bf91659de
Sekhar V <sekharfurture530@gmail.com>
2025-12-19 12:58:49 +0530

    added literals

---
+++ src/ex_Lab003_Literals/Lab017_Literals.java
@@ -0,0 +1,9 @@
+package ex_Lab003_Literals;
+
+public class Lab017_Literals {
+    public static void main(String[] args) {
+        int age = 18;
+//        Datatype-int, age-variable_name =operator(assigment),18 is identifier(Literal)
+        System.out.println("This is Literal in the java");
+    }
+}
---
+++ src/ex_Lab003_Literals/Lab018_LiteralsTypes.java
@@ -0,0 +1,17 @@
+package ex_Lab003_Literals;
+
+public class Lab018_LiteralsTypes {
+    public static void main(String[] args) {
+
+//        Literals types list
+        int age = 18;
+        float deci_num = 15.5f;
+        double sal = 1_00_000;
+        char c = 'A';
+        boolean is_married = true;
+        String name = null;
+
+        System.out.println(age);
+
+    }
+}
