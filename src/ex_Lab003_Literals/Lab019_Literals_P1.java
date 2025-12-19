6267b26b70f1e883bb7d09f9ce51ed598998475d
Sekhar V <sekharfurture530@gmail.com>
2025-12-19 12:58:39 +0530

    added literals

---
+++ src/ex_Lab003_Literals/Lab019_Literals_P1.java
@@ -0,0 +1,11 @@
+package ex_Lab003_Literals;
+
+public class Lab019_Literals_P1 {
+
+    public static void main(String[] args) {
+
+        float pi = 3.14f;
+        float pi2 = 3.14F;
+        float x = 10.00f;
+    }
+}
