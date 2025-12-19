d6ebe9f24694e3530f1f379504930a49f037f414
Sekhar V <sekharfurture530@gmail.com>
2025-12-19 13:08:54 +0530

    Literals->float,char

---
+++ src/ex_Lab003_Literals/Lab020_Boolean_Literals.java
@@ -0,0 +1,12 @@
+package ex_Lab003_Literals;
+
+public class Lab020_Boolean_Literals {
+    public static void main(String[] args) {
+
+        boolean is_married = true;
+        boolean is_married_sekhar = false;
+//        boolean is_married_sekhar = 23;
+
+        System.out.println(is_married);
+    }
+}
---
+++ src/ex_Lab003_Literals/Lab021_Literals_P2.java
@@ -0,0 +1,11 @@
+package ex_Lab003_Literals;
+
+public class Lab021_Literals_P2 {
+
+    public static void main(String[] args) {
+
+        int Enum = 18;
+//        int enum = 20;
+        System.out.println(Enum);
+    }
+}
