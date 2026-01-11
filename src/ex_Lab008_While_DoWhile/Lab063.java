6afd7828683c2d5be908760619762fb7a50bddb0
Sekhar V <sekharfurture530@gmail.com>
2026-01-11 08:04:46 +0530

    Functions or Methods in java with User functions below list 1. Method without return type & without parameter 2. Method without return type & with parameter 3.Method with return type & without parameter 4. Method with return type & with paramter(Most commonly used)

---
+++ src/ex_Lab008_While_DoWhile/Lab063.java
@@ -0,0 +1,19 @@
+package ex_Lab008_While_DoWhile;
+
+public class Lab063 {
+
+    public static void main(String[] args) {
+
+                // your code goes InheritableThreadLocal
+
+                int a = 1;
+
+                do{
+                    System.out.println(a+ " ");
+                    a++;
+                }while(a<=26);
+            }
+
+        }
+
+
